b3654ca119f34b4fff9b452cdec242bc355c46b4
mercy-mwai <mercyneemam@gmail.com>
2024-05-31 12:22:34 +0300

    changes

--- script.js
+++ script.js
@@ -176,3 +176,17 @@ Object.freeze(myFavoriteFootballTeam);
 const {sport,team,year,players}=myFavoriteFootballTeam;
 const{coachName}=myFavoriteFootballTeam.headCoach;
 typeOfSport.textContent=sport;
+teamName.textContent=team;
+worldCupYear.textContent=year;
+headCoach.textContent=coachName;
+
+const setPlayerCards=( arr="players")=>{
+    playerCards.innerHTML += arr.map(({name,position,isCaptain,number,nickName,}) => {
+        `<div class="player-cards><h2>${isCaptain?"(captain)":""}${name}</h2>
+        <p> position:${position}</p>
+
+        </div>"
+
+        `
+    })
+};
